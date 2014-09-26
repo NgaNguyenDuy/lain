311546f1c9afc3c0d8126e6f25c4c5d6c9511f11
Nga Nguyen Duy <kyo1508@gmail.com>
2014-09-27 02:50:58 +0700

    Update mpd.lua

--- widgets/mpd.lua
+++ widgets/mpd.lua
@@ -91,7 +91,7 @@ local function worker(args)
 
                 mpd.id = naughty.notify({
                     preset = mpd_notification_preset,
-                    icon = "~/mpdcover.jpg",
+                    icon = "/home/chris/mpdcover.jpg",
                     replaces_id = mpd.id
                 }).id
             end
